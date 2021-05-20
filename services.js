244a67cdbfe226455f925e704efacf67cf870978
damianmercado32@gmail.com <61065466+rocky-recon@users.noreply.github.com>
2021-05-20 10:14:37 -0700

    update services

--- services.js
+++ services.js
@@ -1,13 +1,13 @@
 function generateUID() {
-  let uid = "";
+  let id = "";
   // generate a unique 10 digits ID as a string
 
   for (let index = 0; index < 10; index++) {
     const rand = Math.floor(Math.random() * 10);
-    uid += rand;
+    id += rand;
   }
 
-  return rand;
+  return id;
 }
 
 // never use the () after the = because if not it will only run once
